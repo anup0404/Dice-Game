7c7086f798bc8a4afa40cc17fffd885d4a4971a6
Anup Kumar <anupkumarggupta@gmail.com>
2023-12-08 22:16:59 +0530

    styled update

--- src/Components/StartGame.jsx
+++ src/Components/StartGame.jsx
@@ -1,36 +1,35 @@
-import React from 'react'
-import styled from "styled-components";
-import { Button } from '../Styled/Button';
+import React from "react";
 
-function StartGame({togle}) {
+import { Button } from "../Styled/Button";
+import { styled } from "styled-components";
+
+function StartGame({ togle }) {
   return (
     <Container>
-      <div> <img src="/images/dices 1.png" />
-      </div> 
-        <div className='content'>
-            <h1>
-                Dice Game
-            </h1>
-            <Button onClick={
-                togle
-            }>Play Now</Button>
-        </div>
+      <div>
+        {" "}
+        <img src="/images/dices 1.png" />
+      </div>
+      <div className="content">
+        <h1>Dice Game</h1>
+        <Button onClick={togle}>Play Now</Button>
+      </div>
     </Container>
-  )
+  );
 }
 
-export default StartGame
+export default StartGame;
 
-const Container=styled.div`
-    max-width:1180px ;
-    display: flex;
-    margin: 0  auto;
-    height: 100vh;
-    align-items:center;
-    .content{
-        h1{
-            font-size:96px;
-            white-space: nowrap;
-        }
+const Container = styled.div`
+  max-width: 1180px;
+  display: flex;
+  margin: 0 auto;
+  height: 100vh;
+  align-items: center;
+  .content {
+    h1 {
+      font-size: 96px;
+      white-space: nowrap;
     }
+  }
 `;
